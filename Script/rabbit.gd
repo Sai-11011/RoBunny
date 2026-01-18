a7e31d86558485c0dbd557e4aaff3755badb05c7
Sai-11011 <23pa1a5723@vishnu.edu.in>
2026-01-18 23:48:16 +0530

    completed rabbit with jump animation and movement illusion

---
+++ Script/rabbit.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const JUMP_VELOCITY = -600.0
+
+func _physics_process(delta: float) -> void:
+	# 1. Apply Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# 2. Handle Jump Input
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# 3. Handle Animation (The Fix)
+	if is_on_floor():
+		# We are on the ground, so we should be running
+		$CollisionShape2D/JumpAnimation.play("run") 
+	else:
+		# We are in the air, so play the jump animation
+		$CollisionShape2D/JumpAnimation.play("jump")
+
+	move_and_slide()
