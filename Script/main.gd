c37ab299d6e9dc3f136051f7fb0fae55ae39e9a1
Sai-11011 <23pa1a5723@vishnu.edu.in>
2026-01-22 23:08:10 +0530

    feat: Implement scoring system

--- Script/main.gd
+++ Script/main.gd
@@ -1,14 +1,24 @@
 extends Node2D
 
+var score = 0
+
 const Glass : PackedScene = preload("res://Scenes/glass.tscn")
 
 func _physics_process(delta: float) -> void:
 	$background/ground.position.x -= 150*delta
 	if $background/ground.position.x <= -960:
 		$background/ground.position.x = -320
+	update_score()
 
 
 func _on_timer_timeout() -> void:
 	var glass = Glass.instantiate()
 	glass.position = Vector2(330,-200)
 	get_parent().add_child(glass)
+
+#score logic
+func _on_score_timeout() -> void:
+	score += 1
+
+func update_score():
+	$score.text = str(score)
